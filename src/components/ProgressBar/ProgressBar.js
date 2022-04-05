a414ada561ef642e084e563220258ffac627f586
arunsharma4084 <97881862+arunsharma4084@users.noreply.github.com>
2022-04-05 08:45:54 +0530

    My solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,68 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  
+  const SIZES = {
+    small: {
+      height: "8px",
+      radius: "4px",
+      padding: 0,
+    },
+
+    medium: {
+      height: "12px",
+      radius: "4px",
+      padding: 0,
+    },
+
+    large: {
+      height: "16px",
+      radius: "8px",
+      padding: "4px",
+    },
+  }
+ 
+  let styles = SIZES[size];
+
+  return (
+    
+  <Wrapper style={{"padding": styles.padding,
+  "--radius": styles.radius, "--height": styles.height,
+  }}>
+  <ComponentWrapper>
+  <Component 
+    role="progressbar"
+    aria-valuenow={value}
+    aria-valuemax="100"
+    aria-valuemin="0"
+  >
+  <VisuallyHidden>{value}</VisuallyHidden>
+  </Component> 
+  </ComponentWrapper>
+  </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+let ComponentWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+`;
+
+const Component = styled.div`
+  height: var(--height);
+  width: ${p => p['aria-valuenow']}%;
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,57 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper>
+    <SelectComponent value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectComponent>
+    <PresentationalBit>
+      {displayedValue}
+      <IconWrapper><Icon id="chevron-down"></Icon> </IconWrapper>
+    </PresentationalBit>
+    </Wrapper>
   );
 };
 
-export default Select;
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const SelectComponent = styled.select`
+ position: absolute;
+ top: 0;
+ left: 0;
+ width: 100%;
+ height: 100%;
+ opacity: 0;
+`;
+
+const PresentationalBit  = styled.div`
+ padding: 12px 16px;
+ padding-right: 52px;
+ color: ${COLORS.gray700};
+ font-size: ${16/16}rem;
+ background-color: ${COLORS.transparentGray15};
+ border-radius: 8px;
+
+ ${SelectComponent}:focus + &{
+   outline: 1px solid ${COLORS.primary};
+ }
+
+ ${SelectComponent}:hover + &{
+   color: ${COLORS.black};
+ }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  height: 24px;
+  top: 0;
+  bottom: 0;
+  right: 4px;
+  margin: auto;
+  pointer-events: none;
+`;
+
+export default Select; 
